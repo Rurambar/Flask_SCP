2bd51af62ac6b573e50ece7289b532008594bdb1
Rurambar <1621819437@qq.com>
2021-11-15 11:40:18 +0800

    完成文件传输

--- BX_CLOUD/auth.py
+++ BX_CLOUD/auth.py
@@ -4,48 +4,48 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
+import hashlib
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/login',methods=('GET',"POST"))
-def login():
-    if request.method=='POST':
-        error=None
-        username=request.form['username']
-        password = request.form['password']
-        if not getDB.check_admin_db(username,password):
-            error="Can't login!"
+# @bp.route('/login',methods=('GET',"POST"))
+# def login():
+#     if request.method=='POST':
+#         error=None
+#         username=request.form['username']
+#         password = request.form['password']
+#         if not getDB.check_admin_db(username,password):
+#             error="Can't login!"
 
-        if error is None:
-            session.clear()
-            session['username']= username
-            return redirect(url_for('index'))
+#         if error is None:
+#             session.clear()
+#             session['username']= username
+#             return redirect(url_for('index'))
 
-        flash(error)
-    return render_template('auth/login.html')
+#         flash(error)
+#     return render_template('auth/login.html')
     
-@bp.before_app_request
-def load_logged_in_user():
-    user_id=session.get('username')
+# @bp.before_app_request
+# def load_logged_in_user():
+#     user_id=session.get('username')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = 'TBX'
+#     if user_id is None:
+#         g.user = None
+#     else:
+#         g.user = 'TBX'
 
 
-@bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
+# @bp.route('/logout')
+# def logout():
+#     session.clear()
+#     return redirect(url_for('index'))
 
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
+# def login_required(view):
+#     @functools.wraps(view)
+#     def wrapped_view(**kwargs):
+#         if g.user is None:
+#             return redirect(url_for('auth.login'))
 
-        return view(**kwargs)
+#         return view(**kwargs)
 
-    return wrapped_view
+#     return wrapped_view
--- BX_CLOUD/blog.py
+++ BX_CLOUD/blog.py
@@ -2,21 +2,61 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-
-from BX_CLOUD.auth import login_required
+from werkzeug.utils import send_file, send_from_directory
+from werkzeug.wrappers.response import Response
+import json
 
 
 import os
 bp = Blueprint('blog', __name__)
 
 
-@bp.route('/')
-@login_required
+@bp.route('/',methods=('GET','POST'))
 def index():
-    posts=os.listdir('BX_CLOUD/files')
+
+    if request.method=='POST':
+        def send_file(file):
+            source_path="BX_CLOUD/files/private/"+file
+            with open(source_path,'rb') as targetfile:
+                while True:
+                    data=targetfile.read(20*1024*1024)
+                    if not data:
+                        break
+                    yield data
+
+        error=None
+        filename=request.form['filename']
+        password=request.form['password']
+        file_db="BX_CLOUD/db/files.json"
+        with open(file_db,'rb') as f:
+            load_dict=json.load(f)
+
+            if filename not in load_dict:
+                error="Can't find the file!"
+            else:
+                if load_dict[filename]==password:
+                        response=Response(send_file(filename),content_type="application/octet-stream")
+                        response.headers["Content-disposition"] = 'attachment; filename=%s' % filename
+                        return response
+                else:
+                    error="False Password!"
+        return error
+    posts=os.listdir('BX_CLOUD/files/public/')
     return render_template('blog/index.html', posts=posts)
 
-@bp.route('/<string:file>/download',methods=('GET','POST'))
-@login_required
+
+
+@bp.route('/download/<string:file>',methods=('GET','POST'))
 def download(file):
-    return "FILE"
+    def send_file():
+        source_path="BX_CLOUD/files/public/"+file
+        with open(source_path,'rb') as targetfile:
+            while True:
+                data=targetfile.read(20*1024*1024)
+                if not data:
+                    break
+                yield data
+
+    response=Response(send_file(),content_type="application/octet-stream")
+    response.headers["Content-disposition"] = 'attachment; filename=%s' % file
+    return response
